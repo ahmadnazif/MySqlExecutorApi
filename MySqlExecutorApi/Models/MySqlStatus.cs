3cf6df3778dc536d7162aa194ef03c1724f8ca49
Nazif Abdullah <ahmadnazif@gmail.com>
2024-07-18 22:29:46 +0800

    Improve model

--- MySqlExecutorApi/Models/MySqlStatus.cs
+++ MySqlExecutorApi/Models/MySqlStatus.cs
@@ -2,16 +2,16 @@
 
 public class MySqlStatus : MySqlStatusBase
 {
-    public string DbIp { get; set; }
-    public string DbName { get; set; }
-    public string DbUserId { get; set; }
+    public string? DbIp { get; set; }
+    public string? DbName { get; set; }
+    public string? DbUserId { get; set; }
 }
 
 public class MySqlStatusBase
 {
-    public string Uptime { get; set; }
+    public string? Uptime { get; set; }
     public DateTime StartTime { get; set; }
-    public string MySqlVersion { get; set; }
+    public string? MySqlVersion { get; set; }
     public int ServerConnectionTimeoutSec { get; set; }
     public int AppConnectionTimeoutSec { get; set; }
     public int DefaultCommandTimeoutSec { get; set; }
