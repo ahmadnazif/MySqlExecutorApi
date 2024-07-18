d6532aeadd6a3085594c039efa789ca244fee9d2
Nazif Abdullah <ahmadnazif@gmail.com>
2024-07-18 22:28:59 +0800

    Put MySqlStatusBase inside MySqlStatus class

--- MySqlExecutorApi/Models/MySqlStatus.cs
+++ MySqlExecutorApi/Models/MySqlStatus.cs
@@ -6,3 +6,13 @@ public class MySqlStatus : MySqlStatusBase
     public string DbName { get; set; }
     public string DbUserId { get; set; }
 }
+
+public class MySqlStatusBase
+{
+    public string Uptime { get; set; }
+    public DateTime StartTime { get; set; }
+    public string MySqlVersion { get; set; }
+    public int ServerConnectionTimeoutSec { get; set; }
+    public int AppConnectionTimeoutSec { get; set; }
+    public int DefaultCommandTimeoutSec { get; set; }
+}
